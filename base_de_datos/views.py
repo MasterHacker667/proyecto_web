7859592a685b0cbc01f692589ed30a656f464a42
masterhacker <masterhacker@kali.masterhacker>
2023-10-21 13:41:00 -0500

    Modificacion, agregue la vista: ibtener_vendedor

--- base_de_datos/views.py
+++ base_de_datos/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Vendedor, User
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
+#Obtener un vendedor por id:
+def obtener_vendedor(request, id_user):
+    try:
+        usuario = User.objects.get(id=id_user)
+        vendedores = Vendedor.objects.all()
+        vendedor = None
+        for i in vendedores:
+            if i.user.id == id_user:
+                vendedor = i
+                break
+        #print(vendedores)
+        if vendedor:
+            vendedor_dict = model_to_dict(vendedor)
+            return JsonResponse(vendedor_dict)
+        else:
+            return JsonResponse({"error": "El usuario no es un vendedor"})
+        return JsonResponse(vendedor)
+    except User.DoesNotExist:
+        return JsonResponse({"error":"El usuario no existe"})
--- dealer/urls.py
+++ dealer/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from base_de_datos import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base_de_datos.urls'))
+    path('', include('base_de_datos.urls')),
+    #Vistas
+    path('vendedor/id_user/<int:id_user>/', views.obtener_vendedor, name="obtener_vendedor_id_user")
 ]
