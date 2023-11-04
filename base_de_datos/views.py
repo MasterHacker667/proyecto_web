7ed3cd3504f6ec7b72eac0d8a4b4be4988d0af4a
masterhacker <masterhacker@kali.masterhacker>
2023-11-04 17:48:23 -0500

    Modificaciones en la vista de logear y autenticar

--- base_de_datos/views.py
+++ base_de_datos/views.py
@@ -62,18 +62,59 @@ def autenticar_usuario(request):
         if user is not None:
             authenticated_user = authenticate(request, username=username, password=password)
             if authenticated_user is not None:
+                try:
+                    cliente = Cliente.objects.get(user=authenticated_user)
+                    user_type = "c"
+                except Cliente.DoesNotExist:
+                    try:
+                        vendedor = Vendedor.objects.get(user=authenticated_user)
+                        user_type = 'v'
+                    except Vendedor.DoesNotExist:
+                        user_type = "0"
+                if user_type == "c":
+                    datos_salida_user = {
+                        "username" : authenticated_user.username,
+                        "id" : authenticated_user.id,
+                        "numero_clave" : cliente.numero_clave,
+                        "cuenta_bancaria" : cliente.cuenta_bancaria,
+                        "exp_month" : cliente.exp_month,
+                        "exp_year" : cliente.exp_year,
+                        "user_type" : user_type
+                    }
+                elif user_type == "v":
+                    datos_salida_user = {
+                        "username" : authenticated_user.username,
+                        "id" : authenticated_user.id,
+                        "numero_clave" : vendedor.numero_clave,
+                        "Clabe" : vendedor.Clabe,
+                        "cuenta_bancaria" : vendedor.cuenta_bancaria,
+                        "exp_month" : vendedor.exp_month,
+                        "exp_year" : vendedor.exp_year,
+                        "stripe_secret_key" : vendedor.stripe_secret_key,
+                        "stripe_publishable_key" : vendedor.stripe_publishable_key,
+                        "disponibilidad" : vendedor.disponibilidad,
+                        "estrellas_prom" : vendedor.estrellas_prom,
+                        "user_type" : user_type
+                    }
+                else:
+                    datos_salida_user = {
+                        "username" : authenticated_user.username,
+                        "id" : authenticated_user.id,
+                        "user_type" : user_type
+                    }
                 return JsonResponse({
                        'message': 'Usuario autenticado en Dealer',
                        'status': True,
+                       'datos_user' : datos_salida_user
                    })
             else:
                 return JsonResponse({
-                       'message': 'Usuario p contraseña incorrectos',
+                       'message': 'Usuario o contraseña incorrectos',
                        'status': False,
                    })
         else:
             return JsonResponse({
-                       'message': 'Usuario o contraseña incorrectos',
+                       'message': 'Usuario inexistente',
                        'status': False,
                    })
     except json.JSONDecodeError:
