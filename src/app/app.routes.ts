c51b2fe1d119ef8c0a88f85020a85a186b6ce0b5
Esmeralda-Salazar <esmeraldasalazar11198@gmail.com>
2024-01-02 22:15:29 -0600

    avance registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { MasVendidosComponent } from './paginas/mas-vendidos/mas-vendidos.compon
 import { OfertasComponent } from './paginas/ofertas/ofertas.component';
 import { PrincipalComponent } from './paginas/principal/principal.component';
 import { CarritoComponent } from './paginas/carrito/carrito.component';
+import { RegistroComponent } from './paginas/registro/registro.component';
 
 export const routes: Routes = [
     { path: 'principal', component: PrincipalComponent},
     { path: 'ofertas', component: OfertasComponent},
     { path: 'mas-vendidos', component: MasVendidosComponent},
     { path: 'login', component: LoginComponent},
-    { path: 'carrito', component: CarritoComponent}
+    { path: 'carrito', component: CarritoComponent},
+    { path: 'registro', component: RegistroComponent}
 ];
 
